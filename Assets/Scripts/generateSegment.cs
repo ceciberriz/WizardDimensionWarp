afa37e0592461b5042db16cfb13729944d8db2b3
Henry Stone <henryvstone@gmail.com>
2018-02-17 16:27:14 -0500

    Added forward buffer so that you always have room in front of you and added support for paths of different lengths

--- Assets/Scripts/generateSegment.cs
+++ Assets/Scripts/generateSegment.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class generateSegment : MonoBehaviour {
 	public GameObject[] pathPrefabs;
+	public float spawnDistance = 15;
 	private GameObject[] pathSegments;
+	public float[] pathLengths;
 
-	private float endLength = 0;
+	private float endLength = -20;
 	private int nextPlatform = 1;
 
 	public Transform player;
@@ -14,12 +16,29 @@ public class generateSegment : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		pathSegments = new GameObject[pathPrefabs.Length];
+		pathLengths = new float[pathPrefabs.Length];
 
 		for (int i = 0; i < pathPrefabs.Length; i++) {
-			GameObject pathSegment = (GameObject)Instantiate (pathPrefabs[i], transform.position, transform.rotation);
+			Vector3 pos = transform.position + new Vector3(0, 0, endLength);
+			GameObject pathSegment = (GameObject)Instantiate (pathPrefabs[i], pos, transform.rotation);
 			pathSegment.transform.SetParent (transform);
+			pathLengths [i] = CalculateZSize (pathSegment);
 			pathSegments[i] = pathSegment;
 		}
+	}
+
+	private float CalculateZSize(GameObject obj)
+	{
+		Bounds bounds = new Bounds(Vector3.zero, Vector3.zero);
+
+		foreach(Renderer renderer in obj.GetComponentsInChildren<Renderer>())
+		{
+			bounds.Encapsulate(renderer.bounds);
+		}
+
+		Vector3 localCenter = bounds.center - this.transform.position;
+		bounds.center = localCenter;
+		return bounds.size.z;
 	}
 		
 	// Update is called once per frame
@@ -27,14 +46,14 @@ public class generateSegment : MonoBehaviour {
 		Vector3 distance = player.position - transform.position;
 		double zDist = Vector3.Dot(distance, transform.forward.normalized);
 
-		if (zDist > endLength) {
+		if (zDist + spawnDistance > endLength) {
 			placeNextPlatform ();
 		}
 	}
 
 	void placeNextPlatform () {
-		pathSegments [nextPlatform].transform.position = new Vector3 (0, 0, endLength + 20);
-		endLength += 20;
+		pathSegments [nextPlatform].transform.position = new Vector3 (0, 0, endLength);
+		endLength += pathLengths[nextPlatform];
 		nextPlatform = (nextPlatform + 1) % pathSegments.Length;
 	}
 }
