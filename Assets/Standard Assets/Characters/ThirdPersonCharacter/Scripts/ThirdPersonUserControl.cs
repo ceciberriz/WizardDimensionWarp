1e990e78b21fb401dfa823d04a4c10d5a85ac6f4
Yujun Jiang <yujunmjiang@yujuns-mbp.devices.brown.edu>
2018-02-24 15:47:42 -0500

    visual

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -4,8 +4,10 @@ using UnityStandardAssets.CrossPlatformInput;
 
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
+    [RequireComponent(typeof(ThirdPersonCharacter))]
     public class ThirdPersonUserControl : MonoBehaviour
     {
+        private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
         private Transform m_Cam;                  // A reference to the main camera in the scenes transform
         private Vector3 m_CamForward;             // The current forward direction of the camera
         private Vector3 m_Move;
@@ -25,6 +27,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
                 // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
             }
 
+            // get the third person character ( this should never be null due to require component )
+            m_Character = GetComponent<ThirdPersonCharacter>();
         }
 
 
@@ -33,9 +37,6 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             if (!m_Jump)
             {
                 m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
-				if (m_Jump) {
-					GetComponent<Rigidbody> ().AddForce (new Vector3 (0, -10, 0));
-				}
             }
         }
 
@@ -51,6 +52,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             
 
             // pass all parameters to the character control script
+            m_Character.Move(m_Move, crouch, m_Jump);
             m_Jump = false;
         }
     }
